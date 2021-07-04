97ff0fb9a60ea1f74b45c38a68aa90fbdb59690c
Ilie Babcenco <ilie.babcenco@gmail.com>
2021-07-04 09:30:42 +0300

    Update sql script to add meals table

--- src/main/resources/db/initDB.sql
+++ src/main/resources/db/initDB.sql
@@ -23,3 +23,14 @@ CREATE TABLE user_roles
     CONSTRAINT user_roles_idx UNIQUE (user_id, role),
     FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE
 );
+
+CREATE TABLE meals
+(
+    id               INTEGER PRIMARY KEY DEFAULT nextval('global_seq'),
+    dateTime         TIMESTAMP                         NOT NULL,
+    description      TEXT                              NOT NULL,
+    calories         INTEGER                           NOT NULL,
+    user_id          INTEGER                           NOT NULL,
+    CONSTRAINT user_date_idx UNIQUE (user_id, dateTime),
+    FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE
+);
